d78e9ade4f9aab52c824895fdb9fb490070fdd95
unknown <sasamilicicbdp@gmail.com>
2022-05-23 10:53:05 +0200

    basic pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Pages from './pages/Pages';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Pages />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Vegetarian from '../components/Vegetarian';
+import Popular from '../components/Popular';
+
+function Home() {
+  return (
+    <div>
+      <Vegetarian />
+      <Popular />
+    </div>
+  );
+}
+
+export default Home;
