ea5ab98c01619d59354c16d0251394facc710584
unknown <sasamilicicbdp@gmail.com>
2022-05-26 09:06:55 +0200

    use params to search / basic

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Searched() {
-  return <div>Searches</div>;
+  const [searchedRecipies, setSearchedRecipies] = useState([]);
+  const { search } = useParams();
+
+  const getSearched = async (name) => {
+    const apiRecipies = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+    const recipies = await apiRecipies.json();
+    setSearchedRecipies(recipies.results);
+
+    console.log(searchedRecipies);
+  };
+
+  useEffect(() => {
+    getSearched(search);
+  }, [search]);
+
+  return (
+    <div>
+      {searchedRecipies.map((recipe) => (
+        <p key={recipe.id}>{recipe.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default Searched;
